7d839b482cb85f93baa423c5580afd8e70b5e026
Alias <2576814881@qq.com>
2020-08-15 16:20:32 +0800

    加入了malloc相关函数

--- include/memory.h
+++ include/memory.h
@@ -1,20 +1,20 @@
 #ifndef _MEMORY_H
 #define _MEMORY_H
 
-#define MEMORY_MIN 0x20000
-#define MEMORY_MAX 0x70000
-#define MEMORY_BLOCK_INFO_SIZE 2+8+4
-
-typedef struct _MemoryBlockInfo{
-    unsigned short isAvailable;
-    unsigned long size;
-    struct _MemoryBlockInfo *pointer; // If this is the first MemoryBlockInfo or isAvailable=1, pointer points to the next availavle MemoryBlokck; else, it points to the previous block.
+typedef struct _MemoryBlockInfo {
+    char isAvailable; // unsigned short isAvailable; // 0 or 1
+    unsigned int size; // Size of the allocated space
+    struct _MemoryBlockInfo* pointer; // If this is the first MemoryBlockInfo or isAvailable=1, pointer points to the next availavle MemoryBlokck; else, it points to the previous block.
 } MemoryBlockInfo;
+
+#define MEM_MIN 0x20000
+#define MEM_MAX 0x70000
+#define MEM_BLOCK_INFO_SIZE sizeof(MemoryBlockInfo)
 
 void malloc_init();
 
-void *malloc(unsigned long size);
+void* malloc(unsigned int size);
 
-void free(void *printer);
+void free(void* printer);
 
 #endif
